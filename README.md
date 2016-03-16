ccd101d1a89ab243f25320ca113a1eeecb738f03
tocology <hwangjun7777@gmail.com>
2016-03-16 22:59:18 +0900

    feat: reset

---
+++ README.md
@@ -0,0 +1,3 @@
+# Algorithms for JAVA
+
+This repository is to gather sources to solve algorithms problems with JAVA.
---
+++ src/KiwiJuiceEasy.java
@@ -0,0 +1,36 @@
+/**
+ * Created by JunHo on 2016. 3. 15..
+ */
+public class KiwiJuiceEasy {
+
+    public static int[] thePouring(int[] capacities, int[] bottles, int[] fromId, int[] toId){
+
+        for(int i=0; i<fromId.length; ++i){
+            int from = fromId[i];
+            int to = toId[i];
+
+            if(capacities[to] >= bottles[to] + bottles[from]) bottles[to] += bottles[from];
+            else {
+                int remains = bottles[to] + bottles[from] - capacities[to];
+                bottles[from] = remains;
+                bottles[to] = capacities[to];
+            }
+        }
+
+        return bottles;
+    }
+
+    public static void main(String[] argv){
+
+        int[] capacities = {700000,800000,900000,1000000};
+        int[] bottles = {478478, 478478, 478478, 478478};
+        int[] fromId = {2,3,2,0,1};
+        int[] toId = {0,1,1,3,2};
+
+        int[] result = thePouring(capacities, bottles, fromId, toId);
+
+        for(int i=0; i<result.length; ++i){
+            System.out.println(result[i] + " ");
+        }
+    }
+}
